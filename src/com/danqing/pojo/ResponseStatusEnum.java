02d4c60980e637c482a1a0b4b66717f33610c0bb
Demo122 <37685456+Demo122@users.noreply.github.com>
2020-04-20 22:28:11 +0800

    添加了 被禁用，没有开启 ,登录成功登录失败 三个枚举变量

--- src/com/danqing/pojo/ResponseStatusEnum.java
+++ src/com/danqing/pojo/ResponseStatusEnum.java
@@ -5,9 +5,13 @@ package com.danqing.pojo;
  * Created by danqing on 2020/4/20 16:42
  */
 public enum ResponseStatusEnum {
-    NO_Authority(-1),
-    Do_SUCCESSFUL(1),
-    Do_FAIELD(2);
+    NO_Authority(-1),  //没有授权
+    NOT_ENABLE(0),     //被禁用，没有开启
+    Do_SUCCESSFUL(1),  //操作成功
+    Do_FAIELD(2),     //操作失败
+    LOGIN_SUCCESS(100), //登录成功
+    LOGIN_FAIL(104);   //登录失败
+
 
     private final int status;
 
