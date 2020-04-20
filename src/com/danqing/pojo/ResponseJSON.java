1e0144719ac66de6bd8454e6267c86ee90e65720
Demo122 <37685456+Demo122@users.noreply.github.com>
2020-04-20 14:22:14 +0800

    创建返回json数据的实体类，主要用户返回请求无授权的url的信息，告诉客户端无权限访问

---
+++ src/com/danqing/pojo/ResponseJSON.java
@@ -0,0 +1,26 @@
+package com.danqing.pojo;
+
+/**
+ * Description: shiro_ssm
+ * Created by danqing on 2020/4/20 14:08
+ */
+public class ResponseJSON {
+    private int code;
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
